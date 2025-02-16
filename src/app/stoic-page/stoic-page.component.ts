916bfc816c4061897de52753b2e24d68da266b6d
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2025-02-15 22:29:26 -0600

    practice filter method

--- src/app/stoic-page/stoic-page.component.ts
+++ src/app/stoic-page/stoic-page.component.ts
@@ -4,10 +4,10 @@ import { Stoic } from '../models/Stoic.model';
 import { StoicAPIService } from '../services/stoicAPI.service';
 import { map } from 'rxjs/operators';
 @Component({
-    selector: 'app-stoic-page',
-    templateUrl: './stoic-page.component.html',
-    styleUrls: ['./stoic-page.component.css'],
-    standalone: false
+  selector: 'app-stoic-page',
+  templateUrl: './stoic-page.component.html',
+  styleUrls: ['./stoic-page.component.css'],
+  standalone: false,
 })
 export class StoicPageComponent implements OnInit {
   stoics: Stoic[] = [];
@@ -19,70 +19,82 @@ export class StoicPageComponent implements OnInit {
   byCategory: boolean = false;
   numPerColumns: number = 1;
 
-  stoicsArray: Stoic [] = [];
-  testArray: Stoic [] = [];
-  wordsArray: string[] = []; 
+  stoicsArray: Stoic[] = [];
+  testArray: Stoic[] = [];
+  wordsArray: string[] = [];
   filterArray: string[] = [];
   filterCat: string[] = [];
+  filteredStoics: string[] = [];
+  searchByLetter: string = '';
 
+  constructor(private router: Router, private stoicService: StoicAPIService) {}
 
-  constructor(
-    private router: Router,
-    private stoicService: StoicAPIService
-  ){}
-
-  ngOnInit(){
-    this.stoicService.getStoics().pipe(
-      map(data => data.map(stoic => ({
-        ...stoic,
-        name: stoic.name.toUpperCase(),
-        description: stoic.description.toUpperCase(),
-        category: stoic.category.toUpperCase()
-      })))).subscribe(data => {
+  ngOnInit() {
+    this.stoicService
+      .getStoics()
+      .pipe(
+        map((data) =>
+          data.map((stoic) => ({
+            ...stoic,
+            name: stoic.name.toUpperCase(),
+            description: stoic.description.toUpperCase(),
+            category: stoic.category.toUpperCase(),
+          }))
+        )
+      )
+      .subscribe((data) => {
         this.testArray = data;
         console.log(this.testArray);
       });
 
-      this.stoicService.getStoics().pipe(
-        map(data => data.map(stoic => stoic.name))
-      ).subscribe(names => {  // names = transformed data that contains the names of the stoics
+    this.stoicService
+      .getStoics()
+      .pipe(map((data) => data.map((stoic) => stoic.name)))
+      .subscribe((names) => {
+        // names = transformed data that contains the names of the stoics
         this.wordsArray = names;
         console.log(this.wordsArray);
       });
 
-      this.stoicService.getStoics().pipe(
-        map(data => data.filter(stoic => stoic.name.startsWith('S'))))
-        .subscribe(filteredNames => {
-          this.filterArray = filteredNames;
-          console.log(this.filterArray);
-        });
+    this.stoicService
+      .getStoics()
+      .pipe(map((data) => data.filter((stoic) => stoic.name.startsWith('S'))))
+      .subscribe((filteredNames) => {
+        this.filterArray = filteredNames;
+        console.log(this.filterArray);
+      });
 
-      this.stoicService.getStoics().pipe(
-        map(data => data.filter(stoic => stoic.category === 'Roman Stoicism')))
-        .subscribe(cat => {
-          this.filterCat = cat;
-          console.log(this.filterCat);
-        })
+    this.stoicService
+      .getStoics()
+      .pipe(
+        map((data) =>
+          data.filter((stoic) => stoic.category === 'Roman Stoicism')
+        )
+      )
+      .subscribe((cat) => {
+        this.filterCat = cat;
+        console.log(this.filterCat);
+      });
 
-   
-  
     this.stoicService.getStoics().subscribe((data) => {
       this.stoicsArray = data;
-      console.log(this.stoicsArray)
+      console.log(this.stoicsArray);
       this.chunkingArray(this.stoicsArray, 2);
-    })
-
-  }
-
-  routeToPage(page: string){
-    this.router.navigate([page]).then(() => {
-      console.log('Navigation to ' + page + ' successful');
-    }).catch((error) => {
-      console.error('Navigation to ' + page + ' failed:', error);
     });
   }
 
-  getStoicbyName(name: string){
+  routeToPage(page: string) {
+    this.router
+      .navigate([page])
+      .then(() => {
+        console.log('Navigation to ' + page + ' successful');
+      })
+      .catch((error) => {
+        console.error('Navigation to ' + page + ' failed:', error);
+      });
+  }
+
+  getStoicbyName(name: string) {
     this.stoicService.getStoicsByName(name).subscribe((data) => {
       this.selectedStoic = data;
       console.log(this.selectedStoic);
@@ -90,33 +102,38 @@ export class StoicPageComponent implements OnInit {
     });
   }
 
-  getStoicbyCategory(category: string){
+  getStoicbyCategory(category: string) {
     this.stoicService.getStoicsByCategory(category).subscribe((data) => {
-      this.selectedCategory =  data;
+      this.selectedCategory = data;
       this.chunkingArray(this.selectedCategory, this.numPerColumns);
       this.byCategory = true;
       console.log(this.selectedCategory);
-    })
+    });
   }
 
   chunkingArray(columnArray: Stoic[], numPerColumns: number) {
-        console.log(columnArray);
-        const chunkedArray = [];
-        for (let i = 0; i < columnArray.length; i += numPerColumns) {
-          const chunk = columnArray.slice(i, i + numPerColumns);
-          chunkedArray.push(chunk);  
-        }
-          columnArray = chunkedArray;
-      }
+    console.log(columnArray);
+    const chunkedArray = [];
+    for (let i = 0; i < columnArray.length; i += numPerColumns) {
+      const chunk = columnArray.slice(i, i + numPerColumns);
+      chunkedArray.push(chunk);
+    }
+    columnArray = chunkedArray;
+  }
 
-
-  changeStoicArray(category: string){
+  changeStoicArray(category: string) {
     let tempArray: Stoic[] = [];
     this.stoicService.getStoicsByCategory(category).subscribe((data) => {
       tempArray = data;
       // Array.prototype.push.apply(this.stoicsArray, tempArray);
       this.stoicsArray.splice(0, this.stoicsArray.length, ...tempArray);
-    })
+    });
   }
 
+  filterStoics(searchByLetter: string) {
+    this.filteredStoics = this.wordsArray.filter(
+      (stoic) => stoic.startsWith(searchByLetter)
+    );
+    console.log(this.filteredStoics);
+  }
 }
